ee2b68162a34d28ea7a8f11aa70355422634fb13
ThapaAmritaS <68316807+ThapaAmritaS@users.noreply.github.com>
2023-11-04 21:26:24 -0500

    Update QR.js
    starting

--- src/QR.js
+++ src/QR.js
@@ -1,23 +1,11 @@
-import logo from './logo.svg';
-import './QR.css';
-
+import { html5qrcode}
 function QR() {
+
   return (
     <div className="QR">
-      <header className="QR-header">
-        <img src={logo} className="QR-logo" alt="logo" />
-        <p>
-          Edit <code>src/QR.js</code> and save to reload.
-        </p>
-        <a
-          className="QR-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        > 
-          Learn React
-        </a>
-      </header>
+      <h1>QR Code Scanning in React </h1>
+        
+      
     </div>
   );
 }
