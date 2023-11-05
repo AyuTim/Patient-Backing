2e8f517ef2a070d28dbec8e26456fdec34e91cc1
AyuTim <67924065+AyuTim@users.noreply.github.com>
2023-11-04 21:09:57 -0500

    did u get

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
+const floorData = {
+  
+}
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* Home/Introduction */}
+      <div className="header">
+        <div className="header-item">
+          <div className="name-container">
+            <center><h1>SwiftCare</h1></center>
+            <div className = "floorData"> 
+              
+             </div>
+          <br></br>
+          </div>
+        </div>
+        <div className="header-item">
+        </div>
+      </div>
     </div>
   );
 }
