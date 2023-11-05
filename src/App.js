0424a81a34e04141b90016749da15f8fd59a8e75
AyuTim <67924065+AyuTim@users.noreply.github.com>
2023-11-04 21:40:17 -0500

    fixed qr

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import React, { useState } from 'react';
+import React from 'react';
 import './App.css';
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'; // Note the import changes
 import QR from './QR';
 
 const floorData = [
@@ -9,27 +9,32 @@ const floorData = [
   {name: 'floor 3'},
   {name: 'floor 4'}
 ]
+
 function App() {
   return (
-    <div className="App">
-      {/* Home/Introduction */}
-      <div className="header">
-        <div className="header-item">
-          <div className="name-container">
-            <center><h1>SwiftCare</h1></center>
-            <div className = "floorData"> 
-             </div>
-          <br></br>
+    <Router>
+      <div className="App">
+        {/* Home/Introduction */}
+        <div className="header">
+          <div className="header-item">
+            <div className="name-container">
+              <center><h1>SwiftCare</h1></center>
+              <div className="floorData"></div>
+              <br></br>
+            </div>
           </div>
         </div>
-      </div>
 
-      <div> 
-        <Link to="/QR">QR</Link>
+        <div> 
+          <Link to="/QR">QR</Link>
+        </div>
+
+        <Routes> {/* Wrapping Routes */}
+          <Route path="/QR" element={<QR />} />
+        </Routes>
       </div>
-    </div>
+    </Router>
   );
 }
-
 
 export default App;
